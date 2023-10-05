e40af8a86c0f976e897f25762a34cf0f787fad3d
marcilustra <theknifebox69@gmail.com>
2023-10-05 18:05:08 +0300

    js script file is made

---
+++ script.js
@@ -0,0 +1,7 @@
+// We will need to add a
+// feature that will dynamically
+// update the page content as
+// the values of the inputs change.
+
+//Like if the user picks a d6 die type
+// A square appears below the input section
