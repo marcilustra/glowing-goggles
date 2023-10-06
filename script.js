3ce2b0b8e4c4ed0aa75dee30a3007c213f5957a8
marcilustra <theknifebox69@gmail.com>
2023-10-06 20:13:33 +0300

    Turns out we didnt need the JS file yet

--- script.js
+++
@@ -1,7 +0,0 @@
-// We will need to add a
-// feature that will dynamically
-// update the page content as
-// the values of the inputs change.
-
-//Like if the user picks a d6 die type
-// A square appears below the input section
